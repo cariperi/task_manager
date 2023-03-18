ebf3bb44e0c792bfcc3a83642996589695762759
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-18 12:10:56 -0400

    feat: add create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,10 @@ class TasksController < ApplicationController
   def index
     @tasks = ['Task 1', 'Task 2', 'Task 3']
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
+
   get '/tasks/', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
 end
