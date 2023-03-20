17cfd22766a1b991711adcec46dc0e66f9a80f89
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-20 15:57:52 -0400

    test: add tests for laundry method and code

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-
+  def laundry?
+    if title == 'laundry'
+      return true
+    elsif description == 'laundry'
+      return true
+    else
+      return false
+    end
+  end
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'instance methods' do
+    describe '#laundry?' do
+      it 'returns true when the title is laundry' do
+        task = Task.create!(title: 'laundry', description: 'clean clothes')
+
+        expect(task.laundry?).to be(true)
+      end
+
+      it 'returns true when the description is laundry' do
+        task = Task.create!(title: 'Clean my clothes', description: 'laundry')
+
+        expect(task.laundry?).to be(true)
+      end
+    end
+  end
+end
